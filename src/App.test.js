6e0b7da3055f538a4497b30078e8763ffddf1577
Abir Alarean <abir.alarean27me@gmail.com>
2022-01-17 19:14:57 +0800

    test change color and text for a button on clicking

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen , fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Button has correct initial condition', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByRole('button' , {name : /change to blue/i }) 
+  expect(button).toHaveStyle({backgroundColor : 'red'})
+  fireEvent.click(button);
+
+  expect(button).toHaveStyle({backgroundColor : 'blue'})
+
+  expect(button.textContent).toBe('Change to red')
 });
+
