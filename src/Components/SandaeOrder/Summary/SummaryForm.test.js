c7f9b7a512b8a8533a3a567c27f067ce2e1fd403
Abir Alarean <abir.alarean27me@gmail.com>
2022-01-19 19:32:32 +0800

    Test and Code popover on hovering the tc label

--- src/Components/SandaeOrder/Summary/SummaryForm.test.js
+++ src/Components/SandaeOrder/Summary/SummaryForm.test.js
@@ -1,4 +1,5 @@
-import {render , screen , fireEvent} from "@testing-library/react";
+import {render , screen, waitForElementToBeRemoved} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from './SummaryForm';
 
 test('The checkbox for T&C is unchecked' , ()=> {
@@ -13,9 +14,28 @@ test('Enable submit order button once the checkbox is checked' , ()=> {
     const checkbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
 
     expect(submitButton).toBeDisabled();
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(submitButton).toBeEnabled();
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(submitButton).toBeDisabled();
 
+})
+
+test('Popover response to hover on checkbox label' , async ()=> {
+    render(<SummaryForm />);
+
+    // Start hidden
+    const nullPopOver =  screen.queryByText(/no ice cream will actually be delivered/i);
+    expect(nullPopOver).not.toBeInTheDocument();
+
+
+    //Apear on mouse over the checkbox label
+    const checkboxLabel = screen.getByText(/terms and conditions/i);
+    userEvent.hover(checkboxLabel);
+    const popOver = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popOver).toBeInTheDocument();
+
+    //disappear on mouse out
+    userEvent.unhover(checkboxLabel);
+    await waitForElementToBeRemoved(()=> screen.queryByText(/no ice cream will actually be delivered/i))
 })
