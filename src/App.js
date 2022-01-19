b793cb1e54cff613fa0ba859c93d9fa20cdda866
Abir Alarean <abir.alarean27me@gmail.com>
2022-01-19 17:59:14 +0800

    Code and test the submit order form button and TC checkbox

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { useState } from 'react';
 import './App.css';
-
-
-export function precedeCamelToSpace(colorName) {
-   return colorName.replace(/\B([A-Z])\B/g, ' $1');
-}
+import SummaryForm from './Components/SandaeOrder/Summary/SummaryForm';
 
 function App() {
 
-  const [ buttonColor , setButtonColor ] = useState('MediumVioletRed') ;
-  const [ disabledButton , setDisabledButton ] = useState(false);
-
-  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed'
 
   return (
     <div className="App-header">
-        <button onClick={()=> setButtonColor(newButtonColor)}
-                style={{backgroundColor : disabledButton ? 'gray' : buttonColor}}
-                disabled={disabledButton}
-        >Change to {precedeCamelToSpace(newButtonColor)}</button>
-        <input type="checkbox"
-                id="disable-button-checkbox"
-                aria-checked={disabledButton}
-                defaultChecked={disabledButton}
-                onChange={(e)=> setDisabledButton(e.target.checked)}/>
-        <label htmlFor="disable-button-checkbox">Disable button</label>
+        <SummaryForm />
     </div>
   );
 }
